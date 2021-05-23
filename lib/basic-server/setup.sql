f208883ed25ee5c40fbff6b5a6dccb4e1d20b455
Mihai Voicescu <mihaivo@intern.pch>
2021-05-23 15:51:53 +0300

    Forgot separator

--- lib/basic-server/setup.sql
+++ lib/basic-server/setup.sql
@@ -6,6 +6,6 @@ CREATE TABLE client
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(64),
     type ENUM('ADMIN', 'USER')
-)
+);
 
-INSERT INTO test_me.client (name, type) VALUES ('gogu', 'ADMIN')
+INSERT INTO test_me.client (name, type) VALUES ('gogu', 'ADMIN');
